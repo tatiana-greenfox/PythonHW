0f25b3a6aef2066224215ae63c39a6368a23f5a3
Tatiana Krylova <1993greenfox@gmail.com>
2020-02-03 15:56:45 +0300

    Внесены изменения в скрипт 2.3 Ecxeption\polish_notation 1. Все арифметические операции вынесены в отдельную функцию arithmetic_operation 2. Общий класс Ecxeption заменен за соответствующие возникающим ошибкам классы (ZeroDivisionError и ValueError)

--- 2.3 Exception/polish_notation.py
+++ 2.3 Exception/polish_notation.py
@@ -3,29 +3,31 @@ def main():
   while True:
     try:
       operation, a, b = input('Введите последовательно через пробел одну из арифметических операций (+, -, /, *) и два числа:').split()
-    except Exception as e:
+    except ValueError as e:
       print(f"Ошибка: {e}\nВозможно вы ввели недостаточно данных, либо забыли поставить пробелы между операндами")
     else:
-      assert operation in ['+', '-', '/', '*'], 'Операция введена не верно!'
-
+      assert operation in ['+', '-', '/', '*'], 'Ошибка! Операция введена не верно!'
       try:
-        a = int(a)
-        b = int(b)
-      except Exception as e:
+        a, b = int(a), int(b)
+      except ValueError as e:
         print(f"Ошибка: {e}\nПроверьте правильно ли вы ввели последние два числа")
       else:
-        if (a < 0) or (b < 0):
-          print('Вы ввели отрицательное число!')
-        elif operation == '+':
-          print(f"Сумма введенных чисел равна: {a + b}")
-        elif operation == '-':
-          print(f"Разность введенных чисел равна: {a - b}")
-        elif operation == '*':
-          print(f"Произведение введенных чисел равно: {a * b}")
-        elif operation == '/':
-          try:
-            print(f"Частное введенных чисел равно: {a / b}")
-          except Exception as e:
-            print(f"Ошибка: {e}\nВы пытаетель делить на ноль")     
+        arithmetic_operations(operation, a , b)
     print()
+
+def arithmetic_operations(operation, a, b):
+  if (a < 0) or (b < 0):
+    print('Вы ввели отрицательное число!')
+  elif operation == '+':
+    print(f"Сумма введенных чисел равна: {a + b}")
+  elif operation == '-':
+    print(f"Разность введенных чисел равна: {a - b}")
+  elif operation == '*':
+    print(f"Произведение введенных чисел равно: {a * b}")
+  elif operation == '/':
+    try:
+      print(f"Частное введенных чисел равно: {a / b}")
+    except ZeroDivisionError:
+      print('Ошибка! Попытка деления на ноль') 
+
 main()
