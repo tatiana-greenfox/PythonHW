a83ca8474b82bea4fac89926101ca295b188cf85
Tatiana Krylova <1993greenfox@gmail.com>
2020-02-13 00:43:27 +0300

    исправление падения при слишком частом обращении к API

--- Spy_games/spy_games.py
+++ Spy_games/spy_games.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 
-import requests
+import requests, time
 
 # API_ID = '7309299'
 access_token = '5f43c35946c516a3f879b50719786fa492274747a73658133de8cf7f0f1ee0f7d351fbb9bc742444bb763'
@@ -9,7 +9,7 @@ version = '5.103'
 # user_screen_name = 'eshmargunov'
 # user_id = '171691064'
 
-def get_response(request, params):
+def requests_get(request, params):
     response = requests.get(request, params) 
     response_json = response.json()
     items_list = response_json['response']['items']
@@ -54,12 +54,7 @@ class User:
 
         self.request = 'https://api.vk.com/method/friends.get'
 
-        return get_response(self.request, self.params)
-
-        # response = requests.get('https://api.vk.com/method/friends.get' , self.params)
-        # response_json = response.json()
-        # items_list = response_json['response']['items']
-        # return items_list
+        return requests_get(self.request, self.params)
     
     def get_groups_list(self, user_id):
         self.params = {
@@ -73,59 +68,71 @@ class User:
 
         self.request = 'https://api.vk.com/method/users.getSubscriptions'
 
-        return get_response(self.request, self.params)
-
-        # response = requests.get('https://api.vk.com/method/users.getSubscriptions' , self.params) 
-        # response_json = response.json()
-        # items_list = response_json['response']['items']
-        # return items_list
+        return requests_get(self.request, self.params)
 
 class Group:
-    pass
+    def get_info_group(self, groups_id_list):
+        self.params = {
+                'access_token': access_token,
+                'v': version,
+                'group_ids': groups_id_list
+            }
+
+        response = requests.get('https://api.vk.com/method/groups.getById' , self.params) 
+        response_json = response.json()
+        pprint(response_json)
+
 
 if __name__ == "__main__":
     user = User()
     user_id = user.get_user_id()
-
     user_groups = user.get_groups_list(user_id)
-    user_groups_id_list = []
-
     user_friends = user.get_friends_list(user_id)
+
+    # получение списка id друзей пользователя,
+    # если у друга нет пометки заблокированн или удален и при этом пользователь может просматривать информацию о нем,
+    # тогда вносим id друга в список
     user_friends_id_list = []
 
-    # получение списка id друзей пользователя
     for friend in user_friends:
-        if (friend.get('deactivated') == 'deleted') or (friend.get('deactivated') == 'banned'):
+        if (friend.get('deactivated') == 'deleted') or (friend.get('deactivated') == 'banned') or ((friend.get('is_closed') == '1') and (friend.get('can_access_closed') == '0')):
             pass
         else:
             user_friends_id_list.append(friend['id'])
 
     # получение списка id групп пользователя
-    for group in user_groups:
-        user_groups_id_list.append(group['id'])
+    # создаем множество, которое будет содержать id групп пользователя
+    user_groups_id_set = set()
 
-    # преобразование списка в множество   
-    user_groups_id_set = set(user_groups_id_list)
+    for group in user_groups:
+        user_groups_id_set.add(group['id'])
 
     # создание объекта для каждого друга пользователя и получение списка групп для каждого друга
-    # Вопрос: правильно ли Я получаю список групп для каждого друга? Если в конце не поставить break выдается ошибка keyerror 'response'
+    friend = User()
     friends_groups_list = []
-    friends_groups_id_list = []
-
-    for friend_id in user_friends_id_list:
-        friend = User()
-        friends_groups_list = friend.get_groups_list(friend_id)
-        break 
-
-    for group in friends_groups_list:
-        friends_groups_id_list.append(group['id'])
+    all_friends_groups_list = []
     
-    # получение множества групп, в которые входит пользователь, но не входит никто из его друзей
-    friends_groups_id_set = set(friends_groups_id_list)
+    print('-' * 10)
+    for friend_id in user_friends_id_list:  
+        try:
+            friends_groups_list = friend.get_groups_list(friend_id)
+            all_friends_groups_list.append(friends_groups_list)
+        except Exception:
+            time.sleep(2)
+            print('-' * 10)
 
-    print(user_groups_id_set.difference(friends_groups_id_set))
+    friends_groups_id_set = set()
+
+    for group_list in all_friends_groups_list:
+        for group in group_list:
+            friends_groups_id_set.add(group['id'])
+
+    difference_groups = list(user_groups_id_set.difference(friends_groups_id_set))
+    groups_list = []
     
-
+    for id_group in difference_groups:
+        id_group_str = str(id_group)
+        groups_list.append(id_group_str)
     
-
-   
+    group = Group()
+    group.get_info_group(groups_list)
